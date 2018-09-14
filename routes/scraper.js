0d0ff0c8656f03bf112e7ed2ce1b4f8089ee4db9
Panagiotis Karypiadis <pan.karyp@gmail.com>
2018-09-14 23:03:59 +0300

    Add source files

---
+++ routes/scraper.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const router = express.Router();
+const path = require("path");
+const osmosis = require('osmosis');
+
+// Home Route
+router.get('/scraper', function (req, res) {
+
+    // Declare the website we want to scrape
+    const website = 'https://dzone.com/';
+    // Osmosis request
+    const dzone = osmosis.get(website);
+    // Set the starting point of Osmosis (high level class name)
+    const articleLocation = '.titles';
+    // #1 Find all the starting points
+    // #2 For every starting point, find the lower level classes
+    // #3 Get the data from those classes
+    dzone
+    .find(articleLocation)
+    .set({'title': '.article-title'})
+    .data(function(dataObject) {
+        // Check if dataObject is empty
+        if (Object.keys(dataObject).length !== 0) {
+            // Print data to the console
+            console.log(dataObject.title);
+        }
+    });
+
+    
+
+});
+
+module.exports = router;
